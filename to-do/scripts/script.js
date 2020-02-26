78ca50a41a0c101278b2941592eb74cde30843c4
Barbara Siegrist <siegrist.barbara@gmail.com>
2020-02-26 16:44:31 +0100

    list-style --done hinzufügen – fehler im HMTL entdeckt. Beginne mit flicken

--- to-do/scripts/script.js
+++ to-do/scripts/script.js
@@ -1,5 +1,7 @@
 var xBtn = document.querySelectorAll('.list__delete');
 var ul = document.querySelector('.list');
+var check = document.querySelectorAll('.list__check');
+var label = document.querySelectorAll('.list__checkbox');
 
 //x-Button für entfernen
 for (var i = 0; i < xBtn.length; i++){
@@ -9,4 +11,18 @@ for (var i = 0; i < xBtn.length; i++){
 function deleteListItem(event){
     var li = event.currentTarget.parentNode;
     ul.removeChild(li);
+}
+
+//Style für erledigt toggeln
+for (var i = 0; i < check.length; i++){
+    check[i].addEventListener('click', function(event){
+        var x = event.target;
+        if (x.checked == true){
+            x.parentNode.parentNode.classList.add('.list__item--done');
+            console.log(x.parentNode.lastChild);
+        } else {
+            x.parentNode.parentNode.classList.remove('.list__item--done');
+            console.log(x.parentNode.parentNode);
+        }
+    })
 }
