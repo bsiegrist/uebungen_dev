aeb83db526b1b02699e950c2141721374d381a27
Barbara Siegrist <siegrist.barbara@gmail.com>
2020-03-20 11:03:58 +0100

    make filters work

--- to-do/scripts/app.js
+++ to-do/scripts/app.js
@@ -4,56 +4,82 @@ const ul = document.querySelector('.list');
 const formfield = document.querySelector('.input__formfield');
 const form = document.querySelector('.input__line');
 const counter = document.querySelector('.counter p')
+const filterShowAll = document.querySelector('#showAll');
+const filterShowToDos = document.querySelector('#showToDos');
+const filterShowDone = document.querySelector('#showDone');
 
-let toDolist = [];
-
+let toDoList = [];
 
 if (localStorage.getItem('list')){
-    toDolist = JSON.parse(localStorage.getItem('list'));
+    toDoList = JSON.parse(localStorage.getItem('list'));
 };
 
 
 //create List from Local Storage
-function renderList(){
+function renderList(filteredList){
     ul.innerHTML = "";
-    toDolist.forEach((element, index) => {
+    filteredList.forEach((element) => {
         let newLi = document.createElement('li');
         newLi.innerHTML = `<div class="list__checkbox">
-                              <input type="checkbox" class="list__checkmark" id="todo-${index}">
-                              <label class="list__label" for="todo-${index}"></label>
+                              <input type="checkbox" class="list__checkmark" id="todo-${element.id}">
+                              <label class="list__label" for="todo-${element.id}"></label>
                            </div>
                            <p class="list__item-name">${element.todoText}</p>
                            <div class="list__delete">
                               <img src="img/cross.svg" alt="delete-button">
                            </div>`;
         newLi.classList.add('list__item');
-        newLi.id = index;
+        newLi.id = element.id;
         ul.appendChild(newLi);
         if (element.done === true){
             newLi.classList.add('list__item--done');
-            document.querySelector(`#todo-${index}`).checked = true;
+            document.querySelector(`#todo-${element.id}`).checked = true;
         };
     })
     //counter
-    let ItemDone = document.querySelectorAll('.list__item--done');
-    counter.innerText = `${(toDolist.length - ItemDone.length)} items left`;
+    let toDoListOpen = toDoList.filter(element => {
+        return element.done === false;
+    });
+    counter.innerText = `${(toDoListOpen.length)} items left`;
+};
+
+function renderListFilter(){
+    //create new list depending on filter
+    if (filterShowToDos.classList.contains('filters__button--active')){
+        let toDoListOpen = toDoList.filter(element => {
+            return element.done === false;
+        });
+        renderList(toDoListOpen)
+    } else if (filterShowDone.classList.contains('filters__button--active')){
+        let toDoListDone = toDoList.filter(element => {
+            return element.done === true;
+        });
+        renderList(toDoListDone);
+    } else {
+    renderList(toDoList);
+    };
 };
 
 //x-Button mit module delegate
 ul.addEventListener('click',Tools.delegate('.list__delete img', (event) => {
-    Tools.removeElement(event.target.parentNode.parentNode);
-    let getID = parseInt(event.target.parentNode.parentNode.id);
-    toDolist.splice(getID, 1);
-    localStorage.setItem('list', JSON.stringify(toDolist));
-    renderList();
+    let getID = parseInt(event.target.parentNode.parentNode.id);  // img  >  div list delete  >  li
+    let getIndex = toDoList.findIndex(element => {
+        return element.id === getID;
+    });
+    toDoList.splice(getIndex, 1);
+    localStorage.setItem('list', JSON.stringify(toDoList));
+    renderListFilter();
 }));
 
 //Style toggeln
 ul.addEventListener('click', Tools.delegate('.list__checkbox input', (event) => {
-    let getID = parseInt(event.target.parentNode.parentNode.id);
-    toDolist[getID].done = !toDolist[getID].done;
-    localStorage.setItem('list', JSON.stringify(toDolist));
-    renderList();
+    let getID = parseInt(event.target.parentNode.parentNode.id);  //input  >  div list checkbox  >  li
+    let getIndex = toDoList.findIndex(element => {
+        return element.id === getID;
+    });
+    toDoList[getIndex].done = !toDoList[getIndex].done;
+    localStorage.setItem('list', JSON.stringify(toDoList));
+    renderListFilter();
 }));
 
 //text eingeben und neues li erstellen
@@ -61,14 +87,43 @@ form.addEventListener('submit', (event) => {
     //standard stoppen
     event.preventDefault(); 
     //push to list
-    toDolist.push({'todoText': formfield.value, 'done': false});
+    toDoList.push({'todoText': formfield.value, 'done': false, 'id': Date.now()});
     //formularfeld leeren
     formfield.value = "";
     //speichern in localStorage
-    localStorage.setItem('list', JSON.stringify(toDolist));
-    //create new list
-    renderList();
+    localStorage.setItem('list', JSON.stringify(toDoList));
+    //create new list depending on filter
+    renderListFilter();
 });
 
+//filterbuttons
+filterShowAll.addEventListener('click', () => {
+    filterShowAll.classList.add('filters__button--active');
+    filterShowToDos.classList.remove('filters__button--active');
+    filterShowDone.classList.remove('filters__button--active');
+    renderList(toDoList);
+})
+
+filterShowToDos.addEventListener('click', () => {
+    filterShowAll.classList.remove('filters__button--active');
+    filterShowToDos.classList.add('filters__button--active');
+    filterShowDone.classList.remove('filters__button--active');
+    let toDoListOpen = toDoList.filter(element => {
+        return element.done === false;
+    });
+    renderList(toDoListOpen);
+})
+
+filterShowDone.addEventListener('click', () => {
+    filterShowAll.classList.remove('filters__button--active');
+    filterShowToDos.classList.remove('filters__button--active');
+    filterShowDone.classList.add('filters__button--active');
+    let toDoListDone = toDoList.filter(element => {
+        return element.done === true;
+    });
+    renderList(toDoListDone);
+})
+
+
 //Liste aufbauen lassen
-renderList();
+renderList(toDoList);
